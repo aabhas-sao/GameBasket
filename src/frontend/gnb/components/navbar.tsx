9ee19eb4cabda59438a2407c95f33fe94b3f966e
Aabhas <aabhassao0@gmail.com>
2021-12-02 15:07:05 +0530

    Chore: check husky working
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- src/frontend/gnb/components/navbar.tsx
+++ src/frontend/gnb/components/navbar.tsx
@@ -135,18 +135,18 @@ export default function Example() {
   return (
     <div className="bg-white">
       {/* Mobile menu */}
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+                                            <Transition.Root show={open} as={Fragment}>
+                                                <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
+                                                <Transition.Child
+                                                    as={Fragment}
+                                                    enter="transition-opacity ease-linear duration-300"
+                                                    enterFrom="opacity-0"
+                                                    enterTo="opacity-100"
+                                                    leave="transition-opacity ease-linear duration-300"
+                                                    leaveFrom="opacity-100"
+                                                    leaveTo="opacity-0"
+                                                >
+                                                    <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
           <Transition.Child
